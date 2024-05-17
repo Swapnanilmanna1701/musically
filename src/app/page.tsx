e4cb050fdbcd051eef0e44aa826039396ef52e41
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-05-17 13:33:41 +0530

    background gradient with aceternity UI setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
+      <FeaturedCourses />
     </main>
   );
 }
