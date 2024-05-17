370c7d3ebe927612a5b64cc20c4e7bf65c91d0b6
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-05-17 21:51:30 +0530

    sticky scroll setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
       <FeaturedCourses />
+      <WhyChooseUs />
     </main>
   );
 }
