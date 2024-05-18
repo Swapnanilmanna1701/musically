83116e12ee9fe3bb5b965dfff7c1d8ce654e6b58
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-05-18 13:43:08 +0530

    redirect page setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonials from "@/components/TestimonialCard";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
-
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
@@ -14,6 +14,7 @@ export default function Home() {
       <MusicSchoolTestimonials />
       <UpcomingWebinars />
       <Instructors />
+      <Footer />
     </main>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,38 +9,39 @@ function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
   return (
     <div
-      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
     >
-      <Menu setActive={setActive}>
-        <Link href="/">
-          <MenuItem
-            setActive={setActive}
-            active={active}
-            item="Home"
-          ></MenuItem>
-        </Link>
-        <MenuItem
-            setActive={setActive}
-            active={active}
-            item="Our Courses"
-          >
-            <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/web-dev">Web Development</HoveredLink>
-            <HoveredLink href="/interface-design">Interface Design</HoveredLink>
-            <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
-            <HoveredLink href="/branding">Branding</HoveredLink>
-            </div>
-          </MenuItem>
-          <MenuItem setActive={setActive} active={active} item="Pricing">
-          <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/hobby">Hobby</HoveredLink>
-            <HoveredLink href="/individual">Individual</HoveredLink>
-            <HoveredLink href="/team">Team</HoveredLink>
-            <HoveredLink href="/enterprise">Enterprise</HoveredLink>
-          </div>
-        </MenuItem>
-      </Menu>
+        <Menu setActive={setActive}>
+            <Link href={"/"}>
+            <MenuItem setActive={setActive} active={active} item="Home">
+            
+            </MenuItem>
+            </Link>
+            <MenuItem
+            setActive={setActive} active={active} item="Our Courses"
+            >
+               <div className="flex flex-col space-y-4 text-sm">
+               <HoveredLink href="/courses">All Courses</HoveredLink>
+            <HoveredLink href="/courses">
+              Basic Music Theory
+            </HoveredLink>
+            <HoveredLink href="/courses">
+              Advanced Composition
+            </HoveredLink>
+            <HoveredLink href="/courses">Songwriting</HoveredLink>
+            <HoveredLink href="/courses">
+              Music Production
+            </HoveredLink>
+               </div>
+            </MenuItem>
+            <Link href={"/contact"}>
+            <MenuItem setActive={setActive} active={active} item="Contact Us">
+            
+            </MenuItem>
+            </Link>
+        </Menu>
     </div>
+
   );
 }
 
