0f110657e551e13cae9b2e1adb9e38edef607c04
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-06-25 21:36:01 +0530

    bug fix

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -10,6 +10,7 @@ function page() {
         <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-white">All courses ({courseData.courses.length})</h1>  
         <div className="flex flex-wrap justify-center">
             {courseData.courses.map((course) => (
+                // eslint-disable-next-line react/jsx-key
                 <CardContainer className="inter-var m-4">
                 <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
                   <CardItem
